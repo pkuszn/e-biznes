06f7a7c6759c79cb80e44d224a81f3243899cccf
pkuszn <patryk.kuszneruk1798@gmail.com>
2024-05-15 20:51:48 +0200

    updated task5

--- 4-go/models/db.go
+++ 4-go/models/db.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"fmt"
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,12 +11,29 @@ import (
 var DB *gorm.DB
 
 func Initialize(dbURL string) *gorm.DB {
-	db, err := gorm.Open(mysql.Open(dbURL), &gorm.Config{})
-	if err != nil {
-		panic("Failed to connect to database")
+	maxAttempts := 100
+	attempt := 0
+
+	for {
+		attempt++
+		fmt.Printf("Attempt %d to connect to the database...\n", attempt)
+		db, err := gorm.Open(mysql.Open(dbURL), &gorm.Config{})
+		if err == nil {
+			fmt.Println("Connected to the database successfully!")
+			DB = db
+			break
+		}
+
+		fmt.Printf("Failed to connect to the database: %v\n", err)
+		if attempt >= maxAttempts {
+			panic("Failed to connect to database2")
+		}
+
+		fmt.Printf("Retrying in 5 seconds...\n")
+		time.Sleep(5 * time.Second)
 	}
-	DB = db
-	return db
+
+	return DB
 }
 
 func Migrate(db *gorm.DB) {
